1fba8da73567590d2d9ea518907ef6067c4983a3
Byabasaija <basaijapascal9@gmail.com>
2021-07-06 23:35:04 +0300

    Implement removeBook functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,36 @@
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bookRemoveAction } from '../actions';
 
 function Book(props) {
-  const { book } = props;
+  const { book, bookRemoveAction } = props;
+
+  const handleRemoveBook = (book) => {
+    bookRemoveAction(book);
+  };
   return (
     <tr>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
+      <td>
+        <button type="submit" onClick={() => handleRemoveBook(book)}>Remove</button>
+      </td>
     </tr>
   );
 }
 
 Book.propTypes = {
   book: PropTypes.objectOf(PropTypes.string),
+  bookRemoveAction: PropTypes.func.isRequired,
 
 };
 
 Book.defaultProps = {
   book: {},
 };
-export default Book;
+
+const mapDispatchToProps = {
+  bookRemoveAction,
+};
+export default connect(null, mapDispatchToProps)(Book);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
